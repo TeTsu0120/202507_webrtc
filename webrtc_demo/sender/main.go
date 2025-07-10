0f158cf53ec1d576e8a76883d414f0dacdcb931e
TeTsu0120 <tetsu.tennis0319@gmail.com>
2025-07-10 10:32:22 +0900

    test

--- webrtc_demo/sender/main.go
+++ webrtc_demo/sender/main.go
@@ -9,8 +9,7 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-    _ "bufio"
-    _ "strings"
+
 	"github.com/gorilla/websocket"
 	"github.com/pion/webrtc/v4"
 	"github.com/pion/webrtc/v4/pkg/media"
@@ -23,17 +22,17 @@ func must(err error) {
 }
 
 func main() {
-	// Connect to signaling server
 	u := url.URL{Scheme: "ws", Host: "signaling-server:8080", Path: "/ws"}
+	fmt.Println("Connecting to signaling server:", u.String())
+
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	must(err)
 	defer conn.Close()
+	fmt.Println("Connected to signaling server")
 
-	// Create WebRTC peer connection
 	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
 	must(err)
 
-	// Create video track
 	videoTrack, err := webrtc.NewTrackLocalStaticSample(
 		webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP8},
 		"video", "pion")
@@ -43,40 +42,55 @@ func main() {
 
 	peerConnection.OnICECandidate(func(c *webrtc.ICECandidate) {
 		if c == nil {
+			fmt.Println("ICE gathering complete")
 			return
 		}
 		payload, _ := json.Marshal(c.ToJSON())
-		conn.WriteMessage(websocket.TextMessage, payload)
+		fmt.Println("Sending ICE candidate:", string(payload))
+		err := conn.WriteMessage(websocket.TextMessage, payload)
+		if err != nil {
+			fmt.Println("Error sending ICE candidate:", err)
+		}
 	})
 
 	peerConnection.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
-		fmt.Println("Connection State:", state)
+		fmt.Println("Connection State changed to:", state)
 	})
 
-	// Read SDP offer
+	fmt.Println("Waiting for SDP offer from signaling server...")
 	_, message, err := conn.ReadMessage()
 	must(err)
+	fmt.Println("Received SDP offer:", string(message))
 
 	var offer webrtc.SessionDescription
 	must(json.Unmarshal(message, &offer))
 	must(peerConnection.SetRemoteDescription(offer))
+	fmt.Println("Set remote description")
 
-	// Create and send SDP answer
 	answer, err := peerConnection.CreateAnswer(nil)
 	must(err)
 	must(peerConnection.SetLocalDescription(answer))
+	fmt.Println("Created and set local SDP answer")
 
 	answerBytes, _ := json.Marshal(answer)
-	conn.WriteMessage(websocket.TextMessage, answerBytes)
+	err = conn.WriteMessage(websocket.TextMessage, answerBytes)
+	if err != nil {
+		fmt.Println("Error sending SDP answer:", err)
+	} else {
+		fmt.Println("Sent SDP answer")
+	}
 
 	// Start sending dummy video frames
 	ticker := time.NewTicker(33 * time.Millisecond) // ~30fps
 	go func() {
 		for range ticker.C {
-			_ = videoTrack.WriteSample(media.Sample{
+			err := videoTrack.WriteSample(media.Sample{
 				Data:     []byte{0x00, 0x00, 0x01, 0x09, 0x10},
 				Duration: time.Second / 30,
 			})
+			if err != nil {
+				fmt.Println("Error writing video sample:", err)
+			}
 		}
 	}()
 
